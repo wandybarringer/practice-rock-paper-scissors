fb79f4abb3e357a4774975efbd1e2422003325bd
William <wabarringer@gmail.com>
2025-11-13 10:37:12 -0800

    Pseudo code done

---
+++ script.js
@@ -0,0 +1,19 @@
+// 1) Ask user for their selection
+// TODO: prompt the user for their choice
+// TODO: EDGE CASE: user doesn't pick r, p or s
+// TODO: EDGE CASE: case sensitivity
+// TODO: EDGE CASE: Empty submission
+// 2) Generate the computer's selection
+// TODO: random selection of r,p,s var index = math.floor(math.random * array.length)
+// 3) Compare selections
+// TODO: if user picks r && comp picks s or user picks s && comp picks p, win
+// TODO: if user and comp pick same, tie
+// TODO: if user picks s && comp picks r or user picks p && comp picks s, loss
+// 4) Display results
+// TODO: show reults in popup
+// 5) Display score
+// TODO: create some variable for win, loss and tie
+// TODO: update variables
+// TODO: display score in popup
+// 6) Ask to play again
+// TODO: Confirm popup to start back at prompt user to choose r p or s
